4e47df1b472f14606891137e20f15d71c3d06a8a
Mos_Sicode <moisesbmenezes123@gmail.com>
2023-11-20 11:19:19 -0300

    Commit

--- MundoInfo/login.js
+++ MundoInfo/login.js
@@ -7,3 +7,35 @@ function changeTab(tabName) {
         document.getElementById('signupForm').style.display = 'block';
     }
 }
+
+function exibirMensagem(mensagem) {
+    const mensagemElement = document.createElement('div');
+    mensagemElement.classList.add('mensagem');
+    mensagemElement.textContent = mensagem;
+    document.body.appendChild(mensagemElement);
+
+    // Limpa a mensagem após alguns segundos (opcional)
+    setTimeout(() => {
+        mensagemElement.remove();
+    }, 5000); // 5000 milissegundos = 5 segundos
+}
+
+document.getElementById('loginForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    // ... (seu código existente para login)
+
+    // Exibe a mensagem de login realizado
+    exibirMensagem('Login realizado com sucesso!');
+});
+
+document.getElementById('signupForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    // ... (seu código existente para cadastro)
+
+    // Exibe a mensagem de cadastro realizado
+    exibirMensagem('Cadastro realizado com sucesso!');
+});
+
+setTimeout(() => {
+    window.location.href = '/index.html';
+}, 8000);
